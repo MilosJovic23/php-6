07eaefe89ef796b6d0e7ab849b1f312204b61197
MilosJovic23 <milos.mj992@gmail.com>
2024-10-30 11:07:03 +0100

    * small changes

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="registracija.php" method="POST">
+            <input type="text" name="pin" >
+            <button type="submit">ukucaj pin</button>
+        </form>
+    </body>
+</html>
