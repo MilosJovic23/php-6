007d81bc7d8f3a0c34a75276664591338ba24c61
MilosJovic23 <milos.mj992@gmail.com>
2024-10-30 11:06:44 +0100

    * homework

---
+++ DomaciForma.php
@@ -0,0 +1,16 @@
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <title>Domaci</title>
+</head>
+<body>
+<form action="Domaci.php" method="POST">
+    <input type="text" name="ime" >
+    <button type="submit">Unesite ime</button>
+</form>
+</body>
+</html>
